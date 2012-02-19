9b66eecfeb55d0b67d28a461bcff341622cb7c89
James OKelly <james@rubyloves.me>
2012-02-18 23:37:02 -0600

    ..

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -40,7 +39,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // Chat client
