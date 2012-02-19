af176cb8ddd1c0180afd4c43b6cf5b6bcf3d6329
James OKelly <james@rubyloves.me>
2012-02-18 23:31:28 -0600

    init

---
+++ app.js
@@ -0,0 +1,81 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes  = require('./routes')
+  , app     = module.exports = express.createServer()
+  , io      = require('socket.io').listen(app)
+  , stylus  = require('stylus');
+
+// For compiling stylus
+function compileMethod(str) {
+  return stylus(str)
+    .set('compress', true);
+};
+  
+// Express Config
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'secret_key' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Chat client
+// usernames which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (data) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+});
---
+++ public/javascripts/chat.js
@@ -0,0 +1,66 @@
+var socket = io.connect('http://localhost:3000');
+
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function(){
+	// call the server-side function 'adduser' and send one parameter (value of prompt)
+	socket.emit('adduser', prompt("What's your name?"));
+	$('#new_message').focus()
+});
+
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function (username, data) {
+	$('#messages').append(
+	  '<div class="message">('+getTimestamp()+') <b>'+username+'</b>: '+data+'</div>');
+	notify()
+});
+
+// on load of page
+$(function(){
+	// when the client clicks SEND
+	$('#send_message').click( function() {
+		var message =  $('#new_message').val();
+		$('#new_message').val('');
+		// tell server to execute 'sendchat' and send along one parameter
+		socket.emit('sendchat', message);
+		$('#new_message').focus()
+	});
+
+	// when the client hits ENTER on their keyboard
+	$('#new_message').keypress(function(e) {
+	  resetTitle();
+		if(e.which == 13) {
+			$(this).blur();
+			$('#send_message').focus().click();
+			$('#new_message').focus()
+		}
+	});
+	
+	$(window).focus(function() {
+    resetTitle();
+  });
+  
+  $('#new_message').focus(function() {
+    resetTitle();
+  });
+});
+
+function getTimestamp() {
+  var timestamp = new Date();
+  return  timestamp.getHours() + ':' + 
+          timestamp.getMinutes() + ':' +
+          timestamp.getSeconds()
+}
+
+function notify() {
+  $('#ding').html(
+    "<embed src='/audio/ding.wav' hidden=true autostart=true loop=false>");
+  notifyTitle();
+}
+
+function notifyTitle() {
+  $(document).attr('title', "New Message");
+}
+
+function resetTitle() {
+  $(document).attr('title', "Conquest Chat");
+}
