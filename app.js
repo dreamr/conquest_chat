e0be92c93231af02eebbee734b342d8f57a89be5
James OKelly <james@rubyloves.me>
2012-02-19 18:07:34 -0600

    added rooms :D

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 /**
  * Module dependencies.
  */
+var express   = require('express')
+  , routes    = require('./routes')
+  , app       = module.exports = express.createServer()
+  , io        = require('socket.io').listen(app)
+  , stylus    = require('stylus')
+  , chat      = require('chat').socketCalls(io)
 
-var express = require('express')
-  , routes  = require('./routes')
-  , app     = module.exports = express.createServer()
-  , io      = require('socket.io').listen(app)
-  , stylus  = require('stylus');
 
 // For compiling stylus
 function compileMethod(str) {
@@ -39,65 +40,6 @@ app.configure('production', function(){
 app.get('/',          routes.index);
 app.get('/viewport',  routes.viewport);
 
-
 // Startup
 app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
-// Chat client
-// usernames which are currently connected to the chat
-var usernames = {};
-
-io.sockets.on('connection', function (socket) {
-
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-	});
-
-	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username){
-		// we store the username in the socket session for this client
-		socket.username = username;
-		// store the default room name
-		socket.room = 'Lobby';
-		// add the client's username to the global list
-		usernames[username] = username;
-		// Send them to the room
-		socket.join('Lobby');
-		
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected');
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected');
-		// update the list of users in chat, client-side
-		io.sockets.to('Lobby').emit('updateusers', usernames);
-	});
-	
-	socket.on('switchRoom', function(newroom){
-		// leave the current room (stored in session)
-		socket.leave(socket.room);
-		// join new room, received as function parameter
-		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-		// update socket session room title
-		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
-	});
-  
-
-  // when the user disconnects.. perform this
-	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
-		io.sockets.to('Lobby').emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-		socket.leave(socket.room);
-	});
-});
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,35 +2,35 @@ var socket = io.connect('http://localhost:80');
 
 // on connection to server, ask for user's name with an anonymous callback
 socket.on('connect', function(){
-	// call the server-side function 'adduser' and send one parameter (value of prompt)
-	socket.emit('adduser', prompt("What's your name?"));
+	// call the server-side function 'addUser' and send one parameter (value of prompt)
+	socket.emit('addUser', prompt("What's your name?"));
 	$('#new_message').focus()
 });
 
-// listener, whenever the server emits 'updatechat', this updates the chat body
-socket.on('updatechat', function (username, data) {
+// listener, whenever the server emits 'updateChat', this updates the chat body
+socket.on('updateChat', function (userName, data) {
 	$('#messages').append(
-	  '<div class="message">('+getTimestamp()+') <b>'+username+'</b>: '+data+'</div>');
+	  '<div class="message">('+getTimestamp()+') <b>'+userName+'</b>: '+data+'</div>');
 	notify()
 });
 
-socket.on('updateusers', function (usernames) {
-  $('#chatters').html('&nbsp;');
-  usernames = keys(usernames).sort();
-  $.each(usernames, function(key, value) {
-  	$('#chatters').append(
+socket.on('updateChatterList', function (userNames) {
+  $('#chatter-list').html('&nbsp;');
+  userNames = keys(userNames).sort();
+  $.each(userNames, function(key, value) {
+  	$('#chatter-list').append(
   	  '<div class="chatter">'+value+'</div>');
 	});
 });
 
-socket.on('updaterooms', function(rooms, current_room) {
-	$('#rooms').empty();
+socket.on('updateRooms', function(rooms, current_room) {
+	$('#room-list').empty();
 	$.each(rooms, function(key, value) {
 		if(value == current_room){
-			$('#rooms').append('<div>' + value + '</div>');
+			$('#room-list').append('<div>' + value + '</div>');
 		}
 		else {
-			$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+			$('#room-list').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
 		}
 	});
 });
@@ -41,8 +41,8 @@ $(function(){
 	$('#send_message').click( function() {
 		var message =  $('#new_message').val();
 		$('#new_message').val('');
-		// tell server to execute 'sendchat' and send along one parameter
-		socket.emit('sendchat', message);
+		// tell server to execute 'sendChat' and send along one parameter
+		socket.emit('sendChat', message);
 		$('#new_message').focus()
 	});
 
