155495044fa758ba4150593b902af615f3965af9
James OKelly <james@rubyloves.me>
2012-02-20 22:57:30 -0600

    ..

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ var express   = require('express')
   , config    = require('./config/config.js').configuration(app);
 
 require('./lib/chat').socketCalls(io);
-
-require('./config/express.js').configure(app)
 require('./config/routing.js').routing(app)
+
+// Express Config
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'secret_key' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 // Startup
 app.listen( config['http_port'] );
